d717a50d67017b74adc5be28e829792258387fab
3uomlkh <3uomlkh@gmail.com>
2025-02-27 19:12:42 +0900

    add: Lv.3 - 명예의 전당 (1)

---
+++ src/lv_3/명예의_전당_1.java
@@ -0,0 +1,27 @@
+package lv_3;
+
+import java.util.PriorityQueue;
+
+public class 명예의_전당_1 {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < score.length; i++) {
+
+            if (queue.size() < k) {
+                queue.add(score[i]);
+                answer[i] = queue.peek();
+                continue;
+            }
+
+            if (queue.peek() < score[i]) {
+                queue.poll();
+                queue.add(score[i]);
+            }
+            answer[i] = queue.peek();
+        }
+
+        return answer;
+    }
+}
